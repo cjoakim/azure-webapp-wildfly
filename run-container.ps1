17e809a55471d898519cd7f9cc29198214b538ff
Chris Joakim <christopher.joakim@gmail.com>
2019-12-13 11:25:03 -0500

    run-container.ps1 - env vars

--- run-container.ps1
+++ run-container.ps1
@@ -5,10 +5,13 @@
 echo 'docker ps before ...'
 docker ps
 
+echo Get-ChildItem Env:AZURE_REDISCACHE_HOST
+echo Get-ChildItem Env:AZURE_REDISCACHE_KEY
+
 echo 'running container ...'
 docker run -d \
-    -e AZURE_REDISCACHE_HOST=$AZURE_REDISCACHE_HOST \
-    -e AZURE_REDISCACHE_KEY=$AZURE_REDISCACHE_KEY \
+    -e AZURE_REDISCACHE_HOST=Get-ChildItem Env:AZURE_REDISCACHE_HOST \
+    -e AZURE_REDISCACHE_KEY=Get-ChildItem Env:AZURE_REDISCACHE_KEY \
     -p 3000:8080 \
     cjoakim/azure-webapp-wildfly:latest
 
