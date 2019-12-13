706aa4dc4dc5e2048b1723252c1b1b04d8c4f4d5
Chris Joakim <christopher.joakim@gmail.com>
2019-12-13 11:06:00 -0500

    Added build-war.ps1

---
+++ build-war.ps1
@@ -0,0 +1,21 @@
+
+# Compile the Java code and package the WAR file for this app.
+# Chris Joakim, Microsoft, 2019/12/13
+
+date  > src/main/resources/build_date.txt
+cat src/main/resources/build_date.txt
+
+whoami > src/main/resources/build_user.txt
+cat src/main/resources/build_user.txt
+
+echo 'executing mvn package ...'
+mvn clean package
+
+echo 'listing contents of target/root.war ...'
+jar tvf target/root.war
+
+echo 'copying war file to docker-dir/deployments directory...'
+cp target/root.war docker-dir/deployments
+
+echo 'listing contents of docker-dir/deployments ...'
+ls -al docker-dir/deployments
