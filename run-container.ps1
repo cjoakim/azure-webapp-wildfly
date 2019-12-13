b82bbb6725092a51023d776a454974082d35d4b0
Chris Joakim <christopher.joakim@gmail.com>
2019-12-13 11:13:20 -0500

    Added run-container.ps1

---
+++ run-container.ps1
@@ -0,0 +1,16 @@
+
+# Execute the Docker container containing the web application.
+# Chris Joakim, Microsoft, 2019/12/13
+
+echo 'docker ps before ...'
+docker ps
+
+echo 'running container ...'
+docker run -d \
+    -e AZURE_REDISCACHE_HOST=$AZURE_REDISCACHE_HOST \
+    -e AZURE_REDISCACHE_KEY=$AZURE_REDISCACHE_KEY \
+    -p 3000:8080 \
+    cjoakim/azure-webapp-wildfly:latest
+
+echo 'docker ps after ...'
+docker ps
