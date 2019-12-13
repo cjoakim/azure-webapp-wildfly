a60f75857c1748e38f962638c81f6f5e6680ea0e
Chris Joakim <christopher.joakim@gmail.com>
2019-12-13 11:35:58 -0500

    run-container.ps1 - line continuation

--- run-container.ps1
+++ run-container.ps1
@@ -9,10 +9,10 @@ echo $env:AZURE_REDISCACHE_HOST
 echo $env:AZURE_REDISCACHE_KEY
 
 echo 'running container ...'
-docker run -d |
-    -e AZURE_REDISCACHE_HOST=$env:AZURE_REDISCACHE_HOST |
-    -e AZURE_REDISCACHE_KEY=$env:AZURE_REDISCACHE_KEY |
-    -p 3000:8080 |
+docker run -d `
+    -e AZURE_REDISCACHE_HOST=$env:AZURE_REDISCACHE_HOST `
+    -e AZURE_REDISCACHE_KEY=$env:AZURE_REDISCACHE_KEY `
+    -p 3000:8080 `
     cjoakim/azure-webapp-wildfly:latest
 
 echo 'docker ps after ...'
